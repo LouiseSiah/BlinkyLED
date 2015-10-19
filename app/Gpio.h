1a8b4741707c1d77d8911b53a22550467f26ee62
LouiseSiah <louisesiah19@gmail.com>
2015-10-19 15:27:56 +0800

    create new Project

---
+++ app/Gpio.h
@@ -0,0 +1,11 @@
+#ifndef __Gpio_H__
+#define __Gpio_H__
+
+#include "stm32f4xx_hal_gpio.h"
+
+#define GPIO_OUTPUT			GPIO_MODE_OUTPUT_PP
+#define GPIO_INPUT			GPIO_MODE_INPUT
+
+void configurePin(int direction, int pinNum, GPIO_TypeDef *port);
+
+#endif // __Gpio_H__
---
+++ app/main.c
@@ -0,0 +1,41 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "Gpio.h"
+//#include <stdint.h>
+
+//void configurePin(GPIO_TypeDef *port, uint32_t pinNumber)
+//{
+//	GPIO_InitTypeDef gpio;
+//	gpio.Pin = GPIO_PIN_13;
+//	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+//	gpio.Speed = GPIO_SPEED_MEDIUM;
+//	HAL_GPIO_Init(port, &gpio);
+//
+//}
+void delay(uint32_t delayCount)
+{
+	while(delayCount != 0)
+		delayCount--;
+}
+
+int main(void)
+{
+//	int a = 4;
+//	double d = 5.6789123;
+
+//	d = d * a + 1.987654321;
+
+	configurePin(GPIO_OUTPUT, GPIO_PIN_13, GPIOG);
+
+    while(1)
+    {
+    	delay(200000);
+    	writeOne(GPIO_PIN_13, GPIOG);
+    	delay(200000);
+    	writeZero(GPIO_PIN_13, GPIOG);
+    }
+//    	turnOnLED(PORTG, LED3);
+//    	turnOffLED(PORTG, LED3);
+//    	HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_SET);
+//		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_RESET);
+}
