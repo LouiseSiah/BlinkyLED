0d0567f0081c12fc17f41fa312e8af2a673a8db3
LouiseSiah <louisesiah19@gmail.com>
2015-10-23 13:44:39 +0800

    blinked other LED

--- app/Gpio.c
+++ app/Gpio.c
@@ -6,11 +6,24 @@ void configurePin(int direction, int pinNum, GPIO_TypeDef *port)
 	GPIO_InitTypeDef GpioInfo;
 
 	__HAL_RCC_GPIOG_CLK_ENABLE();
-	GpioInfo.Pin = pinNum;
-	GpioInfo.Mode = direction;
-	GpioInfo.Speed = GPIO_SPEED_MEDIUM;
-	GpioInfo.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(port, &GpioInfo);
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	int *ptr = (int *) 0x40021800;
+	ptr[0] = 0x14000000;				//MODE.pin14 = output
+	ptr[1] = 0;							//OTYPE.pin14 = push-pull
+	ptr[2] = 0x28000000;				//OSPeED.pin14 = high speed
+
+	int *ptrB = (int *) 0x40020400;
+	ptrB[0] = 0x04000000;				//MODE.pin14 = output
+	ptrB[1] = 0;							//OTYPE.pin14 = push-pull
+	ptrB[2] = 0x08000000;				//OSPeED.pin14 = high speed
+
+//	GpioInfo.Pin = pinNum;
+//	GpioInfo.Mode = direction;
+//	GpioInfo.Speed = GPIO_SPEED_MEDIUM;
+//	GpioInfo.Pull = GPIO_NOPULL;
+
+//	HAL_GPIO_Init(port, &GpioInfo);
 
 }
 
--- app/main.c
+++ app/main.c
@@ -26,13 +26,19 @@ int main(void)
 //	d = d * a + 1.987654321;
 
 	configurePin(GPIO_OUTPUT, GPIO_PIN_13, GPIOG);
+//	configurePin(GPIO_OUTPUT, GPIO_PIN_14, GPIOG);
+//	configurePin(GPIO_OUTPUT, GPIO_PIN_13, GPIOB);
 
     while(1)
     {
     	delay(200000);
     	writeOne(GPIO_PIN_13, GPIOG);
+    	writeOne(GPIO_PIN_14, GPIOG);
+    	writeOne(GPIO_PIN_13, GPIOB);
     	delay(200000);
     	writeZero(GPIO_PIN_13, GPIOG);
+    	writeZero(GPIO_PIN_14, GPIOG);
+    	writeZero(GPIO_PIN_13, GPIOB);
     }
 //    	turnOnLED(PORTG, LED3);
 //    	turnOffLED(PORTG, LED3);
